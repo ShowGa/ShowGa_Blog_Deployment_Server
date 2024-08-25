cd426ba55f3f892f9a8303d38860a14701def03e
ShowGa <showgacareer@gmail.com>
2024-08-25 20:53:47 +0800

    <Modify> Server: generateToken => res.cookie sameSite property change to none

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,11 +4,11 @@ const generateTokenAndSetCookie = (userId, isAdmin, res) => {
   const token = jwt.sign({ id: userId, isAdmin }, process.env.JWT_SECRET);
 
   res.cookie("jwt", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000,
+    maxAge: 60 * 24 * 60 * 60 * 1000,
     // prevent XSS attacks cross-site scripting attacks
     httpOnly: true,
     // CSRF attacks cross-site request forgery attacks
-    sameSite: "strict",
+    sameSite: "none",
     secure: process.env.NODE_ENV !== "development",
   });
 };
